a2e818af4a483db9bb308dbb8bb96403e6a3f598
karimalisahili <kasahili.20@est.ucab.edu.ve>
2025-01-04 12:07:25 -0400

    textfields para settings

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -1,8 +1,45 @@
 import React from "react";
 import Navbar2 from "../components/navbar2";
 import Sidebar from "../components/sidebar";
+import TextField from "../components/textfield";
+import Button from "../components/button";
+import PasswordTextField from "../components/passwordTextField";
+import userIcon from "../assets/userIcon.svg";
+import messageIcon from "../assets/messageIcon.svg";
 
 function Settings() {
+  const [name, setName] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [confirmPassword, setConfirmPassword] = React.useState("");
+
+  const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const name = event.target.value;
+    setName(name);
+  };
+
+  const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const email = event.target.value;
+    setEmail(email);
+  };
+
+  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const password = event.target.value;
+    setPassword(password);
+  };
+
+  const handleConfirmPasswordChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const password = event.target.value;
+    setConfirmPassword(password);
+  };
+
+  function onClick(e: React.MouseEvent<HTMLButtonElement>) {
+    if (name && email && password && confirmPassword) {
+    }
+  }
+
   return (
     <>
       <Navbar2 />
@@ -15,7 +52,40 @@ function Settings() {
             <h3 className="text-h3-bold font-bold text-primary-darkGreen mb-4 text-center">
               Configuración
             </h3>
+            <div className="flex flex-col items-center">
+              <div className="space-y-10 w-3/4">
+                <TextField
+                  label="Nombre"
+                  placeholder="ABC"
+                  value={name}
+                  icon={<img src={userIcon} alt="user icon" />}
+                  onChange={handleNameChange}
+                />
+                <TextField
+                  label="Correo Electrónico"
+                  placeholder="abc@gmail.com"
+                  value={email}
+                  icon={<img src={messageIcon} alt="user icon" />}
+                  onChange={handleEmailChange}
+                />
+                <PasswordTextField
+                  label="Contraseña"
+                  placeholder="clave1234"
+                  value={password}
+                  onChange={handlePasswordChange}
+                />
 
+                <div className="space-y-5">
+                  <Button type="solid" button="secondary" onClick={onClick}>
+                    Guardar
+                  </Button>
+
+                  <Button type="solid" button="secondary" onClick={onClick}>
+                    Borrar Cuenta
+                  </Button>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
